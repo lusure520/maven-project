3953de9dbd58f20fe4875a32f530ac827b5f8812
Yuanlu Deng <dengyuanlu@gmail.com>
2021-06-21 12:32:31 +1200

    fix checkstyle warining

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,11 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  /**
+   * @param someone the name of person.
+   * @return string
+   */
+  public String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
